63ed9b7f7a4037bb77a474314cefd74911315ff4
moodmister <72909580+moodmister@users.noreply.github.com>
2020-12-22 09:43:39 +0200

    Moved css to file

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,46 +7,33 @@
         {{-- Title --}}
         <title>Салон за красота - Антре 18</title>
         {{-- Styles --}}
-        <style>
-            body {
-                color: #000000;
-                background-color: #eee3b5
+        <link href="{{ asset('css/mycss.css') }}" rel="stylesheet" type="text/css">
+        <script>
+            function openNav() {
+              document.getElementById("navbar").style.width = "100%";
             }
-            a {
-                display: inline-block;
-                color: #eee3b5;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
+            
+            function closeNav() {
+              document.getElementById("navbar").style.width = "0%";
             }
-            a:hover {
-                color: #000;
-                background-color: #dac78b
-            }
-            a{
-                text-decoration: none;
-            }
-            nav {
-                margin: 0;
-                padding: 0;
-                overflow: hidden;
-                background-color: #000;
-                
-            }
-        </style>
+            </script>
     </head>
     <body>
         <img src="img/logo-wht.png" alt="logo" width="200px" height="120px">
-        <nav class="navbar">
-            <a href="{{ route('welcome') }}">Начална страница</a>
-            <a href="{{ route('catalogue') }}">Каталог</a>
-            <a href="{{ route('service') }}">Услуги</a>
-            <a href="{{ route('pricelist') }}">Ценоразпис</a>
-            <a href="{{ route('promo') }}">Промо пакети</a>
-            <a href="{{ route('gallery') }}">Галерия</a>
-            <a href="{{ route('aboutme') }}">За мен</a>
-            <a href="{{ route('contact') }}">Контакти</a>
+        <nav id="navbar">
+            <button class="hideMenu" onclick="closeNav()">&times;</button>
+            <div class="overlay-content">
+                <a href="{{ route('welcome') }}">Начална страница</a>
+                <a href="{{ route('catalogue') }}">Каталог</a>
+                <a href="{{ route('service') }}">Услуги</a>
+                <a href="{{ route('pricelist') }}">Ценоразпис</a>
+                <a href="{{ route('promo') }}">Промо пакети</a>
+                <a href="{{ route('gallery') }}">Галерия</a>
+                <a href="{{ route('aboutme') }}">За мен</a>
+                <a href="{{ route('contact') }}">Контакти</a>
+            </div>
         </nav>
+        <button class="showMenu" onclick="openNav()">&#9776;Меню</button>
         @yield('content')
     </body>
 </html>
