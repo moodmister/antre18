fb027d75674b345699900e9c1803b6573995f1e7
moodmister <72909580+moodmister@users.noreply.github.com>
2021-01-05 10:42:14 +0200

    Gallery changes

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -3,11 +3,47 @@
 @section('content')
     
         <h1 class="heading transparent">{{ __('msg.gallery') }}</h1>
-    <div class="container">
+    {{--<div class="container transparent">
         @forelse ($images as $image)
-            <img src="{{ $image->url }}" alt="{{ $image->name }}">
+            <div class="image column-33">
+                <a href="{{ $image->url }}">
+                    <img src="{{ $image->url }}" alt="{{ $image->name }}" style="height: 100%; width:100%">
+                </a>
+            </div>
         @empty
             {{ __('msg.gallery_1') }}
         @endforelse
-    </div>
+    </div>--}}
+
+    <div id="demo" class="carousel slide" data-ride="carousel">
+
+        <!-- Indicators -->
+        <ul class="carousel-indicators">
+          @forelse ($images as $image)
+            <li data-target="#demo" data-slide-to="{{ $loop->index }}"></li>
+          @empty
+              {{ __('msg.gallery_1') }}
+          @endforelse
+        </ul>
+      
+        <!-- The slideshow -->
+        <div class="carousel-inner">
+            @forelse ($images as $image)
+                <div class="carousel-item active">
+                    <img src="{{ $image->url }}" alt="{{ $image->name }}">
+                </div>            
+            @empty
+                {{ __('msg.gallery_1') }}
+            @endforelse
+        </div>
+      
+        <!-- Left and right controls -->
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </a>
+      
+      </div>
 @endsection
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,6 +9,13 @@
         {{-- Styles --}}
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <link href="css/mycss.css" rel="stylesheet" type="text/css">
+        <style>
+            body {
+                font-family: 'Montserrat', sans-serif
+            }
+        </style>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
         {{-- Scripts --}}
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
