862b2d8607399a2764811cd484a628f446f7662d
moodmister <72909580+moodmister@users.noreply.github.com>
2021-01-06 11:35:47 +0200

    Uploads tweak

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -45,15 +45,18 @@ class GeneralController extends Controller
             if ($request->file('image')->isValid()) {
                 $validated = $request->validate([
                     'name' => 'string|max:40',
-                    'image' => 'mimes:jpeg,png|max:1014',
+                    'image' => 'mimes:jpeg,png|max:1024',
                 ]);
+                
                 $extension = $request->image->extension();
                 $request->image->storeAs('/public', $validated['name'].".".$extension);
                 $url = Storage::url($validated['name'].".".$extension);
+
                 $file = File::create([
                    'name' => $validated['name'],
                     'url' => $url,
                 ]);
+
                 Session::flash('success', "Success!");
                 return \Redirect::back();
             }
